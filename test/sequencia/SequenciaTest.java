9973bd8ad10ee3980926bc6a2339aa2d34e02100
camila <camila@camila-HP-G42-Notebook-PC>
2017-07-31 22:52:07 -0300

    Add classe 01 - classe valida

--- test/sequencia/SequenciaTest.java
+++ test/sequencia/SequenciaTest.java
@@ -17,6 +17,12 @@ public class SequenciaTest {
     
     Sequencia teste = new Sequencia();
 
+    @Test
+    public void testClasseValida11() {
+        int[][] saida = {{1, 2, 3}, {6, 7, 8, 9}, {22}};
+        int[] entrada = {1, 2, 3, 6, 7, 8, 9, 22};
+        assertArrayEquals(saida, teste.intervalo(entrada));
+    }
     
     @Test
     public void testClasseValida2() {
